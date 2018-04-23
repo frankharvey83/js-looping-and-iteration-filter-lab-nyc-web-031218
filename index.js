c7feb74396ee29db16d45d424e345b749fe5357f
github username <github email address>
2018-04-23 13:13:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arr, name) {
+  return arr.filter(function(driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  })
+};
+
+function fuzzyMatch(arr, letters){
+  let length = letters.length;
+  return arr.filter(function(driver){
+    return driver.slice(0, length) === letters
+  })
+};
+
+function matchName(arr, name){
+  return arr.filter(function(driver){
+    return driver.name === name;
+  })
+};
